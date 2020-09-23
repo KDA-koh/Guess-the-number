de1c95e7dde01cfee87d046aaad804c549d04715
KDA-koh <63598487+KDA-koh@users.noreply.github.com>
2020-09-23 09:39:20 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+import Constants from 'expo-constants';
+import { Counter } from './components/Counters';
+export default function App(){
+  return(
+ <View>
+    <Counter/>
+ </View> 
+ )
+}
